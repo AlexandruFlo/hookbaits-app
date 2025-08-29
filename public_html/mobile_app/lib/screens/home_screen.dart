2f0750fedf31ee963be96dc5d1a42b9be58263f0
AlexandruFlo <58398877+AlexandruFlo@users.noreply.github.com>
2025-08-29 11:32:07 +0300

    Add files via upload

--- public_html/mobile_app/lib/screens/home_screen.dart
+++ public_html/mobile_app/lib/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../screens/product_list_screen.dart';
 import '../screens/cart_screen.dart';
+import '../screens/profile_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -18,6 +19,7 @@ class _HomeScreenState extends State<HomeScreen> {
     final pages = [
       const ProductListScreen(),
       const CartScreen(),
+      const ProfileScreen(),
     ];
 
     return Scaffold(
@@ -25,8 +27,9 @@ class _HomeScreenState extends State<HomeScreen> {
       bottomNavigationBar: NavigationBar(
         selectedIndex: _currentIndex,
         destinations: const [
-          NavigationDestination(icon: Icon(Icons.storefront_outlined), label: 'Magazin'),
+          NavigationDestination(icon: Icon(Icons.home_outlined), label: 'Acasă'),
           NavigationDestination(icon: Icon(Icons.shopping_cart_outlined), label: 'Coș'),
+          NavigationDestination(icon: Icon(Icons.person_outlined), label: 'Cont'),
         ],
         onDestinationSelected: (i) => setState(() => _currentIndex = i),
       ),
